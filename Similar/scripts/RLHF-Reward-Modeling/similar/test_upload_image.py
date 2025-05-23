b232d80f45a625266ad8d1d5cb21e8996e240cb7
miaobingchen.mbc <miaobingchen.mbc@antgroup.com>
2025-05-23 19:31:11 +0800

    5.23_19.31

--- Similar/scripts/RLHF-Reward-Modeling/similar/test_upload_image.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = "https://api.superbed.cn/upload"
-# 通过链接上传
-# resp = requests.post(url, data={"token": "5c7a6de660d7427ea66cc927a6b8a057", "src": "https://ww1.sinaimg.cn/large/005YhI8igy1fv09liyz9nj30qo0hsn0e"})
-# 通过文件上传
-resp = requests.post(url, data={"token": "5c7a6de660d7427ea66cc927a6b8a057"}, files={"file": open("webpage1.png", "rb")})
-print(resp.json())
